e6a0d8b97c523501d79fb7432380415ffdd3ffea
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-29 00:26:19 +0530

    useState hook js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [count, setCount] = useState(5);
+  // let [count, setCount] = useState(5);
 
-  const addValue = () => {
-    setCount(count+1);
+  // const addValue = () => {
+  //   setCount(count+1);
+  // }
+  // const removeValue = () => {
+  //   setCount(count-1);
+  // }
+  // return (
+  //   <>
+  //     <h1>Chai or Code</h1>
+  //     <h2>Counter : {count}</h2>
+  //     <button onClick={addValue}>Add Value</button><br /><br />
+  //     <button onClick={removeValue}>Remove Value</button>
+  //   </>
+  // )
+  let [count, setCount] = useState(0);
+
+  let incrementValue = () => {
+    count = count + 1;
+    setCount(count);
+    console.log(count);
   }
-  const removeValue = () => {
-    setCount(count-1);
+
+  let decrementValue = () => {
+    count = count - 1;
+    setCount(count);
+    console.log(count);
   }
   return (
     <>
-      <h1>Chai or Code</h1>
-      <h2>Counter : {count}</h2>
-      <button onClick={addValue}>Add Value</button><br /><br />
-      <button onClick={removeValue}>Remove Value</button>
+      <h2>Counter Component</h2>
+      <p>{count}</p>
+      <button onClick={incrementValue}>+</button>
+      <button onClick={decrementValue}>-</button>
     </>
   )
 }
